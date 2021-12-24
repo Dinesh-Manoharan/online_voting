9c1e9b951dd436002ba964aa8f71b8848edb9dbc
Dinesh Manoharan <92298181+Dinesh-Manoharan@users.noreply.github.com>
2021-12-24 12:30:17 +0530

    Update emp_connection.php

--- emp_connection.php
+++ emp_connection.php
@@ -1,5 +1,20 @@
 <?php
-error_reporting(1);
-$con=mysqli_connect('onlinevotingsqlaerver','vote');
+$host = 'onlinevotingsqlaerver.database.windows.net';
+$username = 'sqladmin';
+$password = 'Fastrack#28';
+$db_name = 'vote';
 
+//Initializes MySQLi
+$conn = mysqli_init();
+
+mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+
+// Establish the connection
+mysqli_real_connect($conn, 'onlinevotingsqlaerver.database.windows.net', 'sqladmin', 'Fastrack#28', 'vote', 3306, NULL, MYSQLI_CLIENT_SSL);
+
+//If connection failed, show the error
+if (mysqli_connect_errno())
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
 ?>
