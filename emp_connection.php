139b681bfb5b27caf68ddcc30f04b4557f5b5ad9
Dinesh Manoharan <92298181+Dinesh-Manoharan@users.noreply.github.com>
2021-12-24 16:45:18 +0530

    Update emp_connection.php

--- emp_connection.php
+++ emp_connection.php
@@ -8,7 +8,7 @@ $db_name = 'vote';
 $con = mysqli_init();
 
 // Establish the connection
-mysqli_real_connect($conn , 'onlinevotingsqlaerver.mysql.database.azure.com', 'sqladmin@onlinevotingsqlaerver', 'Fastrack#28', 'vote', 3306, NULL, MYSQLI_CLIENT_SSL);
+mysqli_real_connect($con, 'onlinevotingsqlaerver.mysql.database.azure.com', 'sqladmin@onlinevotingsqlaerver', 'Fastrack#28', 'vote', 3306, NULL, MYSQLI_CLIENT_SSL);
 
 //If connection failed, show the error
 if (mysqli_connect_error())
