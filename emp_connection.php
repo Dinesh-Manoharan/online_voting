aef8dad8bcfaaa89d8b426aef682e09153ac9588
Dinesh Manoharan <92298181+Dinesh-Manoharan@users.noreply.github.com>
2021-12-24 13:54:22 +0530

    Update emp_connection.php

--- emp_connection.php
+++ emp_connection.php
@@ -1,5 +1,5 @@
 <?php
 error_reporting(1);
-$con=mysqli_connect('onlinevotingsqlaerver.database.windows.net', 'sqladmin@onlinevotingsqlaerver', 'Fastrack#28', 'vote_mysqli') or die(mysqli_error());
+$con=mysqli_connect('onlinevotingsqlaerver.database.windows.net', 'sqladmin@onlinevotingsqlaerver', 'Fastrack#28', 'vote') or die(mysqli_error());
 
 ?>
