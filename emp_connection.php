be9af1f268f36322e320daec8d5687dc24f5cacc
Dinesh Manoharan <92298181+Dinesh-Manoharan@users.noreply.github.com>
2021-12-24 16:38:18 +0530

    Update emp_connection.php

--- emp_connection.php
+++ emp_connection.php
@@ -11,7 +11,7 @@ $con = mysqli_init();
 mysqli_real_connect($con, 'onlinevotingsqlaerver.database.windows.net', 'vote (sqladmin)', 'Fastrack#28', 'vote', 3306, NULL, MYSQLI_CLIENT_SSL);
 
 //If connection failed, show the error
-if (mysqli_connect_errno())
+if (mysqli_connect_error())
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
