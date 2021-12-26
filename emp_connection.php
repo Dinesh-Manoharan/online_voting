68a2ff1ef348fe7fb36cb5f562e4ec4cc139fa34
Dinesh Manoharan <92298181+Dinesh-Manoharan@users.noreply.github.com>
2021-12-26 14:31:18 +0530

    Update emp_connection.php

--- emp_connection.php
+++ emp_connection.php
@@ -10,8 +10,8 @@ $con = mysqli_init();
 // Establish the connect
 
 
-$con=mysqli_connect('onlinevotingsqlaerver.mysql.database.azure.com', 'sqladmin' , 'Fastrack#28');
-mysqli_select_db($con, "vote") or die ("no database");  
+$con=mysqli_connect('onlinevotingsqlaerver.mysql.database.azure.com', 'sqladmin' , 'Fastrack#28', 'vote');
+
 
 //If connection failed, show the error
 if (mysqli_connect_error())
